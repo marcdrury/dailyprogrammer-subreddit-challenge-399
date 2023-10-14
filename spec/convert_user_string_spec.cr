4fe9f48fc0376d0198fde5fa7187cdd480a8f784
Marc Drury <marcdrury9@gmail.com>
2023-10-14 03:00:37 +0100

    test: update to use StringConverter module

--- spec/convert_user_string_spec.cr
+++ spec/convert_user_string_spec.cr
@@ -3,7 +3,7 @@ require "../src/convert_user_string.cr"
 
 describe "#convert_user_string" do
 	it "correctly converts a string to an integer" do
-    (convert_user_string "pizza").should eq 78
+    (StringConverter.convert "pizza").should eq 78
 	end
 end
 
