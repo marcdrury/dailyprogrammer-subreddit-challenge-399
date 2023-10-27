a18d0e88b7522d442be179dbe609951a744f3b37
Marc Drury <marcdrury9@gmail.com>
2023-10-27 05:01:09 +0100

    refactor: BonusOne to use helper functions

--- src/bonus_one.cr
+++ src/bonus_one.cr
@@ -1,17 +1,27 @@
 module BonusOne
+	private TARGET_WORD_SUM = 319
+
 	def self.solve : String
-		dictionary_path = __DIR__ + "/enable1_dictionary.txt"
-		words = File.read_lines dictionary_path
-		target_word_sum = 319
-		solution = words.find do |word|
-			word_sum = StringConverter.convert word
-			word_sum == target_word_sum
-		end
-		if solution.nil?
+		words = self.get_words
+		valid_word = self.find_word_with_target_sum words
+
+		if valid_word.nil?
 			raise Exception.new "Failed to solve bonus challenge one. Found no words
 		with the sum of 319."
 		end
-		solution
+		valid_word
+	end
+
+	private def self.get_words : Array(String)
+		dictionary_path = __DIR__ + "/enable1_dictionary.txt"
+		File.read_lines dictionary_path
+	end
+
+	private def self.find_word_with_target_sum(words : Array(String)) : String?
+		valid_word = words.find do |word|
+			word_sum = StringConverter.convert word
+			word_sum == TARGET_WORD_SUM
+		end
 	end
 end
 
