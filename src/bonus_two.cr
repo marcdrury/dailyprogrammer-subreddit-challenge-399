1131db8a1d47ee636a575df67cfab6f90b375633
Marc Drury <marcdrury9@gmail.com>
2023-10-28 02:25:18 +0100

    refactor: use helper function
    File: bonus_two.cr

--- src/bonus_two.cr
+++ src/bonus_two.cr
@@ -3,19 +3,22 @@ require "./string_converter.cr"
 module BonusTwo
 	def self.solve : Int32
 		words = self.get_words
-		count = 0
-		words.each do |word|
-			letter_sum = StringConverter.convert word
-			if letter_sum.odd?
-				count += 1
-			end
-		end
-		count
+		words_with_odd_letter_sum = self.get_words_with_odd_letter_sum words
+		words_with_odd_letter_sum.size
 	end
 
 	private def self.get_words : Array(String)
 		dictionary_path = __DIR__ + "/enable1_dictionary.txt"
 		File.read_lines dictionary_path
+	end
+
+	private def self.get_words_with_odd_letter_sum(
+		words : Array(String)
+	) : Array(String)
+		words.select do |word|
+			letter_sum = StringConverter.convert word
+			letter_sum.odd?
+		end
 	end
 end
 
