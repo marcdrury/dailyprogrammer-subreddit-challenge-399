cad1aea6e975ed984d92db5b05b031a430d8ad68
Marc Drury <marcdrury9@gmail.com>
2023-10-28 03:04:49 +0100

    refactor: bonus_two.cr to use DictionaryReader

--- src/bonus_two.cr
+++ src/bonus_two.cr
@@ -1,15 +1,11 @@
+require "./dictionary_reader.cr"
 require "./string_converter.cr"
 
 module BonusTwo
 	def self.solve : Int32
-		words = self.get_words
+		words = DictionaryReader.get_words
 		words_with_odd_letter_sum = self.get_words_with_odd_letter_sum words
 		words_with_odd_letter_sum.size
-	end
-
-	private def self.get_words : Array(String)
-		dictionary_path = __DIR__ + "/enable1_dictionary.txt"
-		File.read_lines dictionary_path
 	end
 
 	private def self.get_words_with_odd_letter_sum(
